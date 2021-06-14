4767e0ae5d4755dd7a472efd9aadc10df7c2a85f
Prince Dattu <81960348+Dattathreya4@users.noreply.github.com>
2021-06-14 15:53:50 +0530

    Add files via upload

---
+++ app/contact/contact.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent
+{
+  map:string="assets/images/map-icon.png";
+  call:string="assets/images/call-icon.png";
+  email:string="assets/images/email-icon.png"
+
+}
